629eab3346b41ef13e5f9696eb60413ec41fed58
raphaelgpalma <raphaelgpalma@icloud.com>
2024-01-22 15:40:12 -0300

    setting views openai key

--- penguinchat/views.py
+++ penguinchat/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import openai 
+import requests
+import os
+import json
+import render
+from dotenv import load_dotenv
 
-# Create your views here.
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY") 
+
+
+
